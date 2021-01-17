0014a1fadb19bd1fe291904ea78f081e0a411010
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2021-01-17 18:03:49 +0530

    sookie problem withcredentials

--- backend/main/auth.py
+++ backend/main/auth.py
@@ -25,14 +25,14 @@ async def login(request: Request, response: Response, user: schemas.UserCredenti
     else:
         if crud.login(db=db, user=user):
             response.set_cookie(key="Authenticated",
-                                value="True", httponly=True, samesite=None, domain="http://localhost:3000")
+                                value="True", httponly=True, domain="http://127.0.0.1:3000")
             response.set_cookie(key="User",
-                                value=user.id, samesite=None, domain="http://localhost:3000")
+                                value=user.id, domain="http://127.0.0.1:3000",)
 
             return {'detail': 'Authentication Successfull'}
         else:
             response.set_cookie(key="Authenticated",
-                                value="False", httponly=True, samesite=None, domain="http://localhost:3000")
+                                value="False", httponly=True, domain="http://127.0.0.1:3000")
             response.status_code = 401
             return {'detail': 'Authentication Failed'}
 
@@ -41,9 +41,9 @@ async def login(request: Request, response: Response, user: schemas.UserCredenti
 async def logout(response: Response, request: Request):
     if request.cookies.get("Authenticated", "False") == "True":
         response.set_cookie(key="Authenticated", value="False",
-                            httponly=True, samesite=None, domain="http://localhost:3000")
-        response.set_cookie(key="User", samesite=None,
-                            domain="http://localhost:3000")
+                            httponly=True, domain="http://127.0.0.1:3000")
+        response.set_cookie(key="User",
+                            domain="http://127.0.0.1:3000")
 
         return {'detail': 'Successfully Logged Out'}
     else:
--- virtual_queue_sys/src/store/actions/authActions.js
+++ virtual_queue_sys/src/store/actions/authActions.js
@@ -1,69 +1,66 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
-const base_url = "http://127.0.0.1:5000"
+axios.defaults.withCredentials = true;
+
+const base_url = "http://127.0.0.1:5000";
 export const userSignup = (user) => {
-    const url = base_url + "/auth/signup";
-    return (dispatch) => {
-        axios({
-            method: "post",
-            url: url,
-            data: user,
-        })
-            .then((res) => {
-                console.log(res.code);
-                dispatch({ type: "REG_SUCCESS", res });
-
-            }).catch((err) => {
-
-                dispatch({ type: "REG_FAILED", err });
-
-            });
-    };
+  const url = base_url + "/auth/signup";
+  return (dispatch) => {
+    axios({
+      method: "post",
+      url: url,
+      data: user,
+    })
+      .then((res) => {
+        console.log(res.code);
+        dispatch({ type: "REG_SUCCESS", res });
+      })
+      .catch((err) => {
+        dispatch({ type: "REG_FAILED", err });
+      });
+  };
 };
 
 export const userLogin = (user) => {
-    const url = base_url + "/auth/login";
-    return (dispatch) => {
-        axios({
-            method: "post",
-            url: url,
-            data: user,
-        })
-            .then((res) => {
-                console.log(res);
-                localStorage.setItem("login", 'true');
-                localStorage.setItem('user', user.id)
-                dispatch({ type: "AUTH_SUCCESS", res });
-            })
-            .catch((err) => {
-                console.log(err.response.data.code);
-                console.log(err.response.data);
-                dispatch({ type: "AUTH_FAILED", err });
-
-
-            });
-    };
+  const url = base_url + "/auth/login";
+  return (dispatch) => {
+    axios({
+      method: "post",
+      credentials: "include",
+      url: url,
+      data: user,
+    })
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("login", "true");
+        localStorage.setItem("user", user.id);
+        Cookies.set("cookies", res.headers["Set-Cookie"]);
+        dispatch({ type: "AUTH_SUCCESS", res });
+      })
+      .catch((err) => {
+        console.log(err.response.data.code);
+        console.log(err.response.data);
+        dispatch({ type: "AUTH_FAILED", err });
+      });
+  };
 };
 
 export const userLogout = () => {
-
-    const url = base_url + "/auth/logout";
-    return (dispatch) => {
-        axios({
-            method: "get",
-            url: url,
-            data: null,
-        })
-            .then((res) => {
-                console.log(res);
-                localStorage.clear();
-                dispatch({ type: "LOGOUT_SUCCESS" });
-            })
-            .catch((err) => {
-
-                console.log(err);
-
-
-            });
-    };
+  const url = base_url + "/auth/logout";
+  return (dispatch) => {
+    axios({
+      method: "get",
+      url: url,
+      data: null,
+    })
+      .then((res) => {
+        console.log(res);
+        localStorage.clear();
+        dispatch({ type: "LOGOUT_SUCCESS" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 };
