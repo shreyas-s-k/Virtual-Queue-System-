630edafa6396797a4d984087f5ee5f78cb7e5091
Shemin Anto <13395437+sheminanto@users.noreply.github.com>
2021-01-21 12:14:15 +0530

    env file added

--- virtual_queue_sys/src/store/actions/authActions.js
+++ virtual_queue_sys/src/store/actions/authActions.js
@@ -3,82 +3,79 @@ import Cookies from "js-cookie";
 
 axios.defaults.withCredentials = true;
 
-const base_url = "http://127.0.0.1:5000";
+const base_url = process.env.REACT_APP_API_URL;
+console.log(base_url);
 export const userSignup = (user) => {
-    const url = base_url + "/auth/signup";
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "post",
-            url: url,
-            data: user,
-        })
-            .then((res) => {
-                console.log(res.code);
-                dispatch({ type: "REG_SUCCESS", res });
-                user_login({ id: user.id, password: user.password }, dispatch)
-
-            })
-            .catch((err) => {
-                dispatch({ type: "REG_FAILED", err });
-            });
-    };
+  const url = base_url + "/auth/signup";
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "post",
+      url: url,
+      data: user,
+    })
+      .then((res) => {
+        console.log(res.code);
+        dispatch({ type: "REG_SUCCESS", res });
+        user_login({ id: user.id, password: user.password }, dispatch);
+      })
+      .catch((err) => {
+        dispatch({ type: "REG_FAILED", err });
+      });
+  };
 };
 
 function user_login(user, dispatch) {
-    const url = base_url + "/auth/login";
-    dispatch({ type: 'LOADING', action: true })
-    axios({
-        method: "post",
-        url: url,
-        data: user,
-        withCredentials: true,
-        referrerPolicy: "origin-when-cross-origin",
+  const url = base_url + "/auth/login";
+  dispatch({ type: "LOADING", action: true });
+  axios({
+    method: "post",
+    url: url,
+    data: user,
+    withCredentials: true,
+    referrerPolicy: "origin-when-cross-origin",
+  })
+    .then((res) => {
+      console.log(res);
+      localStorage.setItem("login", "true");
+      localStorage.setItem("user", user.id);
+      // console.log(res.headers["Set-Cookie"]);
+      // Cookies.set("Authenticated", "True");
+      dispatch({ type: "AUTH_SUCCESS", res });
     })
-        .then((res) => {
-            console.log(res);
-            localStorage.setItem("login", "true");
-            localStorage.setItem("user", user.id);
-            // console.log(res.headers["Set-Cookie"]);
-            // Cookies.set("Authenticated", "True");
-            dispatch({ type: "AUTH_SUCCESS", res });
-        })
-        .catch((err) => {
-            console.log(err.response);
-            console.log(err.response);
-            dispatch({ type: "AUTH_FAILED", err });
-        });
-};
-
+    .catch((err) => {
+      console.log(err.response);
+      console.log(err.response);
+      dispatch({ type: "AUTH_FAILED", err });
+    });
+}
 
 export const userLogin = (user) => {
-
-
-    return (dispatch) => {
-        user_login(user, dispatch)
-    }
-    //     dispatch({ type: 'LOADING', action: true })
-    //     axios({
-    //         method: "post",
-    //         url: url,
-    //         data: user,
-    //         withCredentials: true,
-    //         referrerPolicy: "origin-when-cross-origin",
-    //     })
-    //         .then((res) => {
-    //             console.log(res);
-    //             localStorage.setItem("login", "true");
-    //             localStorage.setItem("user", user.id);
-    //             // console.log(res.headers["Set-Cookie"]);
-    //             // Cookies.set("Authenticated", "True");
-    //             dispatch({ type: "AUTH_SUCCESS", res });
-    //         })
-    //         .catch((err) => {
-    //             console.log(err.response);
-    //             console.log(err.response);
-    //             dispatch({ type: "AUTH_FAILED", err });
-    //         });
-    // };
+  return (dispatch) => {
+    user_login(user, dispatch);
+  };
+  //     dispatch({ type: 'LOADING', action: true })
+  //     axios({
+  //         method: "post",
+  //         url: url,
+  //         data: user,
+  //         withCredentials: true,
+  //         referrerPolicy: "origin-when-cross-origin",
+  //     })
+  //         .then((res) => {
+  //             console.log(res);
+  //             localStorage.setItem("login", "true");
+  //             localStorage.setItem("user", user.id);
+  //             // console.log(res.headers["Set-Cookie"]);
+  //             // Cookies.set("Authenticated", "True");
+  //             dispatch({ type: "AUTH_SUCCESS", res });
+  //         })
+  //         .catch((err) => {
+  //             console.log(err.response);
+  //             console.log(err.response);
+  //             dispatch({ type: "AUTH_FAILED", err });
+  //         });
+  // };
 };
 // export const userLogin = (user) => {
 //     const url = base_url + "/auth/login";
@@ -105,22 +102,22 @@ export const userLogin = (user) => {
 // };
 
 export const userLogout = () => {
-    const url = base_url + "/auth/logout";
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "get",
-            url: url,
-            data: null,
-        })
-            .then((res) => {
-                console.log(res);
-                localStorage.clear();
-                // Cookies.set("Authenticated", "False");
-                dispatch({ type: "LOGOUT_SUCCESS" });
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    };
+  const url = base_url + "/auth/logout";
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "get",
+      url: url,
+      data: null,
+    })
+      .then((res) => {
+        console.log(res);
+        localStorage.clear();
+        // Cookies.set("Authenticated", "False");
+        dispatch({ type: "LOGOUT_SUCCESS" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 };
--- virtual_queue_sys/src/store/actions/eventActions.js
+++ virtual_queue_sys/src/store/actions/eventActions.js
@@ -2,160 +2,159 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const base_url = "http://127.0.0.1:5000";
+const base_url = process.env.REACT_APP_API_URL;
+console.log(base_url);
 export const createEvent = (event) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "POST",
-            url: base_url + "/event",
-            data: { ...event, user_id: localStorage.getItem("user") },
-        })
-            .then((res) => {
-                console.log("createEvent", res);
-                dispatch({ type: "CREATE_EVENT_SUCCESS", event });
-            })
-            .catch((err) => {
-                dispatch({ type: "CREATE_EVENT_ERROR", err });
-                console.log("createEventError", err.response);
-            });
-    };
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "POST",
+      url: base_url + "/event",
+      data: { ...event, user_id: localStorage.getItem("user") },
+    })
+      .then((res) => {
+        console.log("createEvent", res);
+        dispatch({ type: "CREATE_EVENT_SUCCESS", event });
+      })
+      .catch((err) => {
+        dispatch({ type: "CREATE_EVENT_ERROR", err });
+        console.log("createEventError", err.response);
+      });
+  };
 };
 
 export const createSlots = (slot, event_id) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "POST",
-            url: base_url + "/event/slot",
-            data: slot,
-        })
-            .then((res) => {
-                console.log("createSlot", res);
-                dispatch({ type: "CREATE_SLOT_SUCCESS" });
-                viewSlots(event_id, dispatch);
-            })
-            .catch((err) => {
-                dispatch({ type: "CREATE_SLOT_ERROR" })
-                console.log("createSlotError", err.response);
-            });
-    };
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "POST",
+      url: base_url + "/event/slot",
+      data: slot,
+    })
+      .then((res) => {
+        console.log("createSlot", res);
+        dispatch({ type: "CREATE_SLOT_SUCCESS" });
+        viewSlots(event_id, dispatch);
+      })
+      .catch((err) => {
+        dispatch({ type: "CREATE_SLOT_ERROR" });
+        console.log("createSlotError", err.response);
+      });
+  };
 };
 
 function viewSlots(event_id, dispatch) {
-    console.log("ViewSlots fn", event_id);
+  console.log("ViewSlots fn", event_id);
 
-    axios({
-        method: "GET",
-        url: base_url + "/event/slot/" + event_id,
+  axios({
+    method: "GET",
+    url: base_url + "/event/slot/" + event_id,
+  })
+    .then((res) => {
+      console.log("viewSlot", res);
+      dispatch({ type: "VIEW_SLOT_SUCCESS", res });
     })
-        .then((res) => {
-            console.log("viewSlot", res);
-            dispatch({ type: "VIEW_SLOT_SUCCESS", res });
-        })
-        .catch((err) => {
-            console.log("viewSlotError", err.response);
-        });
+    .catch((err) => {
+      console.log("viewSlotError", err.response);
+    });
 }
 
 export const finishCreateEvent = () => {
-    return (dispatch) => {
-        dispatch({ type: "HOST_SUCCESS" });
-    };
+  return (dispatch) => {
+    dispatch({ type: "HOST_SUCCESS" });
+  };
 };
 
 export const viewUserEvents = (user_id) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "GET",
-            url: base_url + "/event/user/" + user_id,
-        })
-            .then((res) => {
-                console.log("ViewUserEvents", res);
-                dispatch({ type: "VIEW_USER_EVENTS", res });
-            })
-            .catch((err) => {
-                console.log("viewUserEventsError", err.response);
-            });
-    };
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "GET",
+      url: base_url + "/event/user/" + user_id,
+    })
+      .then((res) => {
+        console.log("ViewUserEvents", res);
+        dispatch({ type: "VIEW_USER_EVENTS", res });
+      })
+      .catch((err) => {
+        console.log("viewUserEventsError", err.response);
+      });
+  };
 };
 
 export const viewEventDetails = (event_id) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "GET",
-            url: base_url + "/event/" + event_id,
-        })
-            .then((res) => {
-                console.log("ViewEventDetails", res);
-                viewSlots(event_id, dispatch);
-                dispatch({ type: "VIEW_EVENT_DETAILS", res });
-            })
-            .catch((err) => {
-                console.log("ViewEventDetailsError", err.response);
-            });
-    };
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "GET",
+      url: base_url + "/event/" + event_id,
+    })
+      .then((res) => {
+        console.log("ViewEventDetails", res);
+        viewSlots(event_id, dispatch);
+        dispatch({ type: "VIEW_EVENT_DETAILS", res });
+      })
+      .catch((err) => {
+        console.log("ViewEventDetailsError", err.response);
+      });
+  };
 };
 
 export const clearEvents = () => {
-    return (dispatch) => {
-        dispatch({ type: "CLEAR_EVENTS" });
-    };
+  return (dispatch) => {
+    dispatch({ type: "CLEAR_EVENTS" });
+  };
 };
 
 export const bookEvent = (slot_details) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "POST",
-            url: base_url + "/event/attend",
-            data: { ...slot_details, user_id: localStorage.getItem("user") },
-        })
-            .then((res) => {
-                console.log("bookEvent", res);
-                dispatch({ type: "BOOK_EVENT_SUCCESS", res });
-
-            })
-            .catch((err) => {
-                dispatch({ type: "BOOK_EVENT_ERROR", err });
-                console.log("bookEventError", err.response);
-            });
-    };
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "POST",
+      url: base_url + "/event/attend",
+      data: { ...slot_details, user_id: localStorage.getItem("user") },
+    })
+      .then((res) => {
+        console.log("bookEvent", res);
+        dispatch({ type: "BOOK_EVENT_SUCCESS", res });
+      })
+      .catch((err) => {
+        dispatch({ type: "BOOK_EVENT_ERROR", err });
+        console.log("bookEventError", err.response);
+      });
+  };
 };
 
-
 export const viewParticipants = (event_id) => {
-    return (dispatch) => {
-        axios({
-            method: "GET",
-            url: base_url + "/event/participants/" + event_id,
-        })
-            .then((res) => {
-                console.log("ViewParticipants", res);
-                dispatch({ type: "VIEW_PARTICIPANTS", res });
-            })
-            .catch((err) => {
-                console.log("viewParticipantsError", err.response);
-            });
-    };
+  return (dispatch) => {
+    axios({
+      method: "GET",
+      url: base_url + "/event/participants/" + event_id,
+    })
+      .then((res) => {
+        console.log("ViewParticipants", res);
+        dispatch({ type: "VIEW_PARTICIPANTS", res });
+      })
+      .catch((err) => {
+        console.log("viewParticipantsError", err.response);
+      });
+  };
 };
 
 export const viewParticipantEvents = (user_id) => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING', action: true })
-        axios({
-            method: "GET",
-            url: base_url + "/event/registered-events/" + user_id,
-        })
-            .then((res) => {
-                console.log("ViewPartitcipantEvents", res);
-                dispatch({ type: "VIEW_PARTICIPANT_EVENTS", res });
-            })
-            .catch((err) => {
-                console.log("viewPartitcipantEventsError", err.response);
-                dispatch({ type: "VIEW_PARTICIPANT_EVENTS_ERROR", err });
-            });
-    };
+  return (dispatch) => {
+    dispatch({ type: "LOADING", action: true });
+    axios({
+      method: "GET",
+      url: base_url + "/event/registered-events/" + user_id,
+    })
+      .then((res) => {
+        console.log("ViewPartitcipantEvents", res);
+        dispatch({ type: "VIEW_PARTICIPANT_EVENTS", res });
+      })
+      .catch((err) => {
+        console.log("viewPartitcipantEventsError", err.response);
+        dispatch({ type: "VIEW_PARTICIPANT_EVENTS_ERROR", err });
+      });
+  };
 };
